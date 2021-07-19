96d565534939335a498386eaf62ce3889d888e8d
nishimototo <dkspm2436@gmail.com>
2021-07-19 07:53:47 +0000

    ad5終了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,9 +2,9 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     if @range == "User"
-      User.looks(params[:word], params[:search])
+      @users = User.looks(params[:word], params[:search])
     else
-      Book.looks(params[:word], params[:search])
+      @books = Book.looks(params[:word], params[:search])
     end
   end
 end
