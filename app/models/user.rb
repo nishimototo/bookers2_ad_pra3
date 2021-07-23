16743da23143959d68377f3d7836d634b334dcc7
nishimototo <dkspm2436@gmail.com>
2021-07-23 14:26:47 +0000

    Userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
-
+　has_many :followings, through: :active_relationships, source: :follower
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follo
 
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
